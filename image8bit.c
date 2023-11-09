f79f8e30c5718994cf5566479b938a00b31979c4
Nunoua112994 <120404252+Nunoua112994@users.noreply.github.com>
2023-11-09 18:05:44 +0000

    solved G

--- image8bit.c
+++ image8bit.c
@@ -341,7 +341,12 @@ int ImageValidRect(Image img, int x, int y, int w, int h) { ///
 // The returned index must satisfy (0 <= index < img->width*img->height)
 static inline int G(Image img, int x, int y) {
   int index;
-  // Insert your code here!
+  static inline int G(Image img, int x, int y) {
+  int index;
+  index = y*img->witdh+x;
+  assert (0 <= index && index < img->width*img->height);
+  return index;
+}
   assert (0 <= index && index < img->width*img->height);
   return index;
 }
